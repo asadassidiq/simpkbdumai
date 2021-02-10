5e158d3800ac7007dc9d2ba4d009612a437aa9a0
asadassidiq <asad.sidiq2@gmail.com>
2021-02-10 22:27:20 +0700

    amprah

--- resources/views/admin/cetak/lmbrpemeriksaan_print.blade.php
+++ resources/views/admin/cetak/lmbrpemeriksaan_print.blade.php
@@ -65,7 +65,7 @@
                 </div>
                 <div class="col-sm-2"></div>
             </div>
-            <div class="row" style="line-height: 12px;font-size: 14px;margin-top: 2px;margin-left: 5px;">
+            <div class="row" style="line-height: 16px;font-size: 14px;margin-top: 2px;margin-left: 5px;">
                 <div class="col-sm-8">
                     <div class="col-sm-3"></div>
                     <div class="col-sm-6">
