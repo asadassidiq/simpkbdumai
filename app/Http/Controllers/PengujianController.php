117f0f304003a3039b93ae33299170b138c32921
asadassidiq <asad.sidiq2@gmail.com>
2021-03-03 14:13:44 +0700

    tunda

--- app/Http/Controllers/PengujianController.php
+++ app/Http/Controllers/PengujianController.php
@@ -105,7 +105,7 @@ class PengujianController extends Controller
         if ($identitaskendaraan->kodepenerbitans_id == '8') {
             $statuspenerbitan = '2';
         }elseif ($identitaskendaraan->kodepenerbitans_id == '7' || $identitaskendaraan->kodepenerbitans_id == '12') {
-            $kode = Pendaftaran::where('pendaftarans.identitaskendaraan_id',$identitaskendaraan->identitaskendaraan_id)->where('kodepenerbitans_id','!=','7')->orderBy('pendaftarans.id','desc')->first();
+            $kode = Pendaftaran::where('pendaftarans.identitaskendaraan_id',$identitaskendaraan->identitaskendaraan_id)->where('kodepenerbitans_id','!=','7')->where('kodepenerbitans_id','!=','12')->orderBy('pendaftarans.id','desc')->first();
             $statuspenerbitan = $kode->kodepenerbitans_id;
         }else{
             $statuspenerbitan = $identitaskendaraan->kodepenerbitans_id;
